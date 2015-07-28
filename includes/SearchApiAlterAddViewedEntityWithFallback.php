3c372c166cc943456b45cc6711cdbcd19bd2d9d0
Alexander Tkachev <alex.leksat@gmail.com>
2015-07-28 13:12:50 +0300

    Option for excluding entities with empty title_field (SIG-575)

--- includes/SearchApiAlterAddViewedEntityWithFallback.php
+++ includes/SearchApiAlterAddViewedEntityWithFallback.php
@@ -14,6 +14,7 @@ class SearchApiAlterAddViewedEntityWithFallback extends SearchApiAlterAddViewedE
    * {@inheritdoc}
    */
   public function alterItems(array &$items) {
+    $entity_type = $this->index->getEntityType();
     $languages = language_list();
 
     // Save languages.
@@ -41,6 +42,20 @@ class SearchApiAlterAddViewedEntityWithFallback extends SearchApiAlterAddViewedE
         drupal_static_reset('field_language');
         drupal_static_reset('language_fallback_get_candidates');
 
+        // Exclude items with empty title if required.
+        if (!empty($this->options['exclude_empty_title']) && isset($item->title_field)) {
+          // First get the field language, then try to get field items with this
+          // language. This guarantees correct work if language fallback is
+          // used.
+          // Also, it's important to do this after static caches are cleared and
+          // global languages are replaced.
+          $field_language = field_language($entity_type, $item, 'title_field', $item->language);
+          if (field_get_items($entity_type, $item, 'title_field', $field_language) === FALSE) {
+            unset($items[$key]);
+            continue;
+          }
+        }
+
         $_items = array(&$item);
         parent::alterItems($_items);
       }
@@ -59,6 +74,23 @@ class SearchApiAlterAddViewedEntityWithFallback extends SearchApiAlterAddViewedE
    */
   public function getViewMode() {
     return isset($this->options['mode']) ? $this->options['mode'] : NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configurationForm() {
+    $form = parent::configurationForm();
+    $this->options += array(
+      'exclude_empty_title' => FALSE,
+    );
+    $form['exclude_empty_title'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Exclude entities with empty title_field'),
+      '#description' => t('This works only on entities having title_field field.'),
+      '#default_value' => $this->options['exclude_empty_title'],
+    );
+    return $form;
   }
 
 }
